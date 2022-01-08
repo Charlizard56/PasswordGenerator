9a7babae689861c720e380ce99c9f781364f6b3e
DM <85085855+Charlizard56@users.noreply.github.com>
2022-01-08 17:41:58 -0500

    Update + Size Entry - Size Variable = Organized Code

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from tkinter import*
 import Create
 
 root = Tk()
-# Variables
 
-times = 12
+# Variables
 t = False
 s = False
 n = False
@@ -12,17 +11,26 @@ low = False
 upp = False
 password = ""
 
+##Methods############################
+
 
 def gen_password():
     try:
+        # Get Entry
+        size = Size_Entry.get()
+        # Set Entry to int
+        s = int(size)
+
         dis()
         Generate_Button.update()
         print("Gen Started...")
         Password_Entry.delete(0, "end")
-        password = Create.create(times, t, s, n, low, upp)
+        password = Create.create(s, t, s, n, low, upp)
         Password_Entry.insert(END, password)
     except:
+        Password_Entry.delete(0, "end")
         Password_Entry.insert(END, "Failed to generate")
+        dis()
     finally:
         dis()
         print("Gen_Password Finished")
@@ -38,22 +46,41 @@ def dis():
         Generate_Button["text"] = "Generate"
         Generate_Button["bg"] = "LightGreen"
 
+##Set Title###################################
 
-# Set Title
+
 root.title("CL's Password Generator")
 
+
 # Label Widget
+
+
 My_Label = Label(root, text="Password: ")
 
 # Button
-Generate_Button = Button(root, text="Generate", bg="LightGreen", width=26, command=(lambda: gen_password()))
+
+
+Generate_Button = Button(root, text="Generate", bg="LightGreen", width=25, command=(lambda: gen_password()))
 
 # Entry Widget
-Password_Entry = Entry(root, bd=4)
 
-# Add to Screen
-Generate_Button.pack(side=TOP)
-My_Label.pack(side=LEFT)
-Password_Entry.pack(side=RIGHT)
+
+Password_Entry = Entry(root, bd=4, width=30)
+Size_Entry = Entry(root, bd=5, width=2)
+
+###Add to Screen###########################
+# Buttons
+
+
+Generate_Button.grid(column=1,row=0)
+
+# Labels
+
+
+My_Label.grid(column=0,row=1)
+
+# Entries
+Password_Entry.grid(column=1,row=1)
+Size_Entry.grid(column=0,row=0)
 
 root.mainloop()
